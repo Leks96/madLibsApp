db2695a4e28b75c81aba3c3278621ae9ea3cf021
Saidi Sodiq <saidisodiq96@gmail.com>
2023-02-10 18:49:44 +0100

    let's test madLib out

--- app.js
+++ app.js
@@ -1,28 +1,55 @@
 "use script"
 
-let originalStory= `In the land of the Maasai, in Southwest Kenya,
+`In the land of the Maasai, in Southwest Kenya,
 there is a girl named Rebecca.
 Rebecca lives near a large national park, where
 there are golden lions, and big grey elephants.
 Rebecca's family keep cattle, sheep and goats and
 depend on farming the land for food for their
 animals. Rebecca goes to school and helps her
-family care for the animals when she is home.`;
+family care for the animals when she is home.`
+
+let originalStory= `In the land of the ${userInputs[0]}, in ${userInputs[1]} ${userInputs[2]},
+there is a ${userInputs[3]} named ${userInputs[4]}.
+${userInputs[4]} lives near a ${userInputs[5]}, where
+there are ${userInputs[6]}, and ${userInputs[7]}.
+${userInputs[4]}'s family keep ${userInputs[14]}, ${userInputs[15]} and ${userInputs[16]} and
+depend on farming the land for food for their
+animals. ${userInputs[4]} ${userInputs[8]} to ${userInputs[9]} and ${userInputs[10]} ${userInputs[11]}
+family ${userInputs[12]} for the ${userInputs[13]} when ${userInputs[11]} is home.`;
 
 let userInputs=[];
 
 let numberOfQuestions= 17;
 let questionCounter= 0;
 
-for(questionCounter= i; i >=0; i++){
-    
+let questionArray= [
+    "Enter your Tribe",
+    "Your Geopolitical zone",
+    "Your Country",
+    "Your gender (boy or girl)",
+    "Your name",
+    "Your locality",
+    "Amazing sites near your locality",
+    "Other amazing sites near your locality",
+    "Enter a verb",
+    "Enter a noun",
+    "Enter another Verb",
+    "Enter a pronoun (his or her)",
+    "Enter another noun (collective noun)",
+    "Enter another noun (collective noun)",
+    "Enter any animal",
+    "Enter another animal",
+    "Enter another animal"
+]
+
+for(let i= numberOfQuestions; i >=0; i--){
+    console.log(questionArray[questionCounter]);
+   userInputs.push(prompt=(questionArry[questionCounter]));
+   console.log(userInputs);
+   questionCounter++;
+   numberOfQuestions--;
 }
 
-console.log(`In the land of the ${userInputs[0, tribe]}, in ${userInputs[1, geopoliticalzone]} ${userInputs[2, country]},
-there is a ${userInputs[3, gender(boy or girl)]} named ${userInputs[4, Name]}.
-${userInputs[4, Name]} lives near a ${userInputs[5, locality]}, where
-there are ${userInputs[6, amazingsites]}, and ${userInputs[7, otheramazingsites]}.
-${userInputs[4, Name]}'s family keep ${userInputs[14, anyAnimal]}, ${userInputs[15, anyAnimal]} and ${userInputs[16, anyAnimal]} and
-depend on farming the land for food for their
-animals. ${userInputs[4, Name]} ${userInputs[8, verb]} to ${userInputs[9, noun]} and ${userInputs[10, anotherVerb]} ${userInputs[11, pronoun]}
-family ${userInputs[12, noun]} for the ${userInputs[13, anothernoun]} when ${userInputs[11, pronoun]} is home.`)
+console.log(originalStory);
+document.write(originalStory);
