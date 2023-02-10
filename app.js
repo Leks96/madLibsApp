669295d1218a8160da9784dc49efc8ca8df5db50
Saidi Sodiq <saidisodiq96@gmail.com>
2023-02-10 18:01:04 +0100

    Added mad-lib replacement variables to story

---
+++ app.js
@@ -0,0 +1,21 @@
+"use script"
+
+let originalStory= `In the land of the Maasai, in Southwest Kenya,
+there is a girl named Rebecca.
+Rebecca lives near a large national park, where
+there are golden lions, and big grey elephants.
+Rebecca's family keep cattle, sheep and goats and
+depend on farming the land for food for their
+animals. Rebecca goes to school and helps her
+family care for the animals when she is home.`;
+
+let userInputs=[];
+
+console.log(`In the land of the ${userInputs[0, tribe]}, in ${userInputs[1, geopoliticalzone]} ${userInputs[2, country]},
+there is a ${userInputs[3, gender(boy or girl)]} named ${userInputs[4, Name]}.
+${userInputs[4, Name]} lives near a ${userInputs[5, locality]}, where
+there are ${userInputs[6, amazingsites]}, and ${userInputs[7, otheramazingsites]}.
+${userInputs[4, Name]}'s family keep ${userInputs[14, anyAnimal]}, ${userInputs[15, anyAnimal]} and ${userInputs[16, anyAnimal]} and
+depend on farming the land for food for their
+animals. ${userInputs[4, Name]} ${userInputs[8, verb]} to ${userInputs[9, noun]} and ${userInputs[10, anotherVerb]} ${userInputs[11, pronoun]}
+family ${userInputs[12, noun]} for the ${userInputs[13, anothernoun]} when ${userInputs[11, pronoun]} is home.`)
