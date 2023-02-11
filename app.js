8ef210e71456d5d55bdb0a394c322f7924aa4f40
Saidi Sodiq <saidisodiq96@gmail.com>
2023-02-11 05:14:04 +0100

    madLib app project

--- app.js
+++ app.js
@@ -1,5 +1,62 @@
 "use script"
 
+//Total number of questions to be asked
+let questionTotal = 14;
+
+//question counter to know which question we are on
+let questionCounter = 0;
+
+//Test Logic
+let questionArray = [
+    "Please enter your Tribe","Please enter your Geopolitical zone",
+    "Please enter your country name",
+    `Please enter your gender (boy or girl)`,
+    `Please enter your name`,
+    `Please enter a verb (third person singular)`,
+    `Please enter any wild animal you know`,
+    `Please enter another wild animal`,
+    `Please enter a livestock (cattle, chicken etc..)`,
+    `Please enter another livestock`,
+    `Please enter another livestock`,
+    `Please enter another verb (third person singular)`,
+    `Please enter a personal pronoun (he or her)`,
+    `Please enter another verb (feed, cater etc..)`,
+    `Please enter a pronoun (he or she)`,
+]
+
+//Takes the user response
+let userInputs = [];
+for(let i = questionTotal; i >= 0; i--){
+    //console.log(i);
+    //console.log(questionArray[questionCounter]+ `... (${questionTotal} questions left)`);
+
+    //adds users response to the user inputs array
+    userInputs.push(
+        prompt(questionArray[questionCounter]+ `... (${questionTotal} questions left)`)
+        );
+        //console.log(userInputs);
+        questionCounter ++;
+        questionTotal --;
+}
+
+alert('All done! Ready for your totally-accurate self story')
+
+let originalStory = (
+    `<h2>In the land of the ${userInputs[0]}, in ${userInputs[1]} ${userInputs[2]},
+    there is a ${userInputs[3]} named ${userInputs[4]}.</h2>
+    <h2>${userInputs[4]} ${userInputs[5]} near a large national park, where
+    there are golden ${userInputs[6]}, and big grey ${userInputs[7]}.</h2>
+    <h2>${userInputs[4]}'s family keep ${userInputs[8]}, ${userInputs[9]} and ${userInputs[10]} and
+    depend on farming the land for food for their animals.</h2> 
+    <h2>${userInputs[4]} ${userInputs[11]} to school 
+    and helps ${userInputs[12]} family ${userInputs[13]} for the animals when ${userInputs[14]} is home</h2>`
+)
+
+console.log(originalStory);
+document.write(originalStory);
+
+
+
 /*`In the land of the Maasai, in Southwest Kenya,
 there is a girl named Rebecca.
 Rebecca lives near a large national park, where
@@ -8,48 +65,3 @@ Rebecca's family keep cattle, sheep and goats and
 depend on farming the land for food for their
 animals. Rebecca goes to school and helps her
 family care for the animals when she is home.`*/
-windows.onload = () => {
-
-let numberOfQuestions= 17;
-let questionCounter= 0;
-
-let questionArray= [
-    "Enter your Tribe",
-    "Your Geopolitical zone",
-    "Your Country",
-    "Your gender (boy or girl)",
-    "Your name",
-    "Your locality",
-    "Amazing sites near your locality",
-    "Other amazing sites near your locality",
-    "Enter a verb",
-    "Enter a noun",
-    "Enter another Verb",
-    "Enter a pronoun (his or her)",
-    "Enter another noun (collective noun)",
-    "Enter another noun (collective noun)",
-    "Enter any animal",
-    "Enter another animal",
-    "Enter another animal"
-]
-
-let userInputs = [];
-for (let i = numberOfQuestions; i >= 0; i--){
-    console.log(questionArray[questionCounter]);
-   userInputs.push(
-    prompt=(questionArray[questionCounter])
-    );
-   console.log(userInputs);
-   questionCounter++;
-   numberOfQuestions--;
-}
-
-let originalStory= `<h2>In the land of the ${userInputs[0]}, in ${userInputs[1]} ${userInputs[2]}, there is a ${userInputs[3]} named ${userInputs[4]}.</h2>
-<h2>${userInputs[4]} lives near a ${userInputs[5]}, where there are ${userInputs[6]}, and ${userInputs[7]}.</h2>
-<h2>${userInputs[4]}'s family keep ${userInputs[14]}, ${userInputs[15]} and ${userInputs[16]} and depend on farming the land for food for their animals.</h2>
-<h2>${userInputs[4]} ${userInputs[8]} to ${userInputs[9]} and ${userInputs[10]} ${userInputs[11]}
-family ${userInputs[12]} for the ${userInputs[13]} when ${userInputs[11]} is home.</h2>`;
-
-console.log(originalStory);
-document.write(originalStory);
-};
